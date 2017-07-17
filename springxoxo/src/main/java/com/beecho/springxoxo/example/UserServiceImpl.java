73d2cf385831f44125f5407a21b00c1e6f191634
张春举 <zhangchunju@autohome.com.cn>
2017-07-17 15:51:37 +0800

    --

---
+++ springxoxo/src/main/java/com/beecho/springxoxo/example/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package com.beecho.springxoxo.example;
+
+import com.beecho.springxoxo.model.User;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by Administrator on 2017/7/17.
+ */
+public class UserServiceImpl implements UserService {
+
+    private final AtomicLong idGen = new AtomicLong();
+
+    public Long registerUser(User user) {
+        return idGen.incrementAndGet();
+    }
+}
